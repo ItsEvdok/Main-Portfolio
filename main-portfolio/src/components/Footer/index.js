47ce215f088fca5f37f1a4bbb0fff205c416c8d6
ItsEvdok <semionaevdokimov@icloud.com>
2021-07-11 17:16:25 -0400

    set up about, header, footer sections

---
+++ main-portfolio/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <div>
+            <p>GitHub</p>
+            <p>LinkedIn</p>
+        </div>
+    )
+}
+
+export default Footer
