ce7d84f34076c482e8c2d3502497dbe91584521b
ItsEvdok <semionaevdokimov@icloud.com>
2021-07-19 21:02:37 -0400

    working on rendering projects

--- main-portfolio/src/components/Portfolio/index.js
+++ main-portfolio/src/components/Portfolio/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import projects from './projects';
 
 export default function Portfolio() {
     return (
-        <section class="text-center">
+        <section class="align-center">
             <h2 class="m-4"> Projects </h2>
+            <div className='d-flex flex-wrap flex-row justify-content-center'>
+            {projects.map((project) => (
+                <h3>{project}</h3>
+            ))}
+            </div>
         </section>
     )
 };
---
+++ main-portfolio/src/components/Portfolio/projects/index.js
@@ -0,0 +1,14 @@
+const projects = [
+    {
+        name: 'StayBuzzin',
+        repo: '',
+        url: '',
+    },
+    {
+        name: 'StayBuzzin',
+        repo: '',
+        url: '',
+    }
+];
+
+export default projects;
