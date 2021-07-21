768ed3bb8f7728a741707023a5c0e33d8914cf1d
ItsEvdok <semionaevdokimov@icloud.com>
2021-07-21 18:33:04 -0400

    started setting up contact form

--- main-portfolio/src/components/Contact/index.js
+++ main-portfolio/src/components/Contact/index.js
@@ -1,11 +1,64 @@
-import React from 'react';
+import React from "react";
+import Form from "react-bootstrap/Form";
+import { validateEmail } from "../utils/helpers";
 
-function Contact() {
-    return (
-        <section class="text-center">
-            <h2 class="m-4"> Contact Me </h2>
-        </section>
-    )
+export default function Contact() {
+
+  const initialState = { name: "", email: "", message: "" };
+  const [entry, setFormState] = useState(initialState);
+  const { name, email, message } = entry;
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleOnChange = (e) => {
+    setFormState({ ...entry, [e.target.name]: e.target.value });
+    if (e.target.name === "email") {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage("Provided email is not valid.");
+      } else {
+        setErrorMessage("");
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage("");
+      }
+    }
+    if (!errorMessage) {
+      setFormState({ ...entry, [e.target.name]: e.target.value });
+    }
+  };
+
+  function handleSubmit(e) {
+    const isValid = validateEmail(email);
+    const refresh = () => {
+      alert(`${message} Sent!`);
+      window.location = "/";
+    };
+    e.preventDefault();
+    errorMessage
+      ? alert(errorMessage)
+      : !name && email && message
+      ? alert("Name is required")
+
+      : !email && name && message
+      ? alert("Email is required")
+
+      : !message && email && name
+      ? alert("Message is required")
+
+      : !message && !email && !name
+      ? alert("Fields can not be blank!")
+
+      : !isValid
+      ? alert("Your email is invalid")
+      : refresh();
+  }
+
+  return (
+    <section class="text-center">
+      <h2 class="m-4"> Contact Me </h2>
+    </section>
+  );
 }
-
-export default Contact;
