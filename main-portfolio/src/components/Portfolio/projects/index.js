32d370aaefed0c3689c245464844d5769a3177e3
ItsEvdok <semionaevdokimov@icloud.com>
2021-08-01 23:30:11 -0400

    Update index.js

--- main-portfolio/src/components/Portfolio/projects/index.js
+++ main-portfolio/src/components/Portfolio/projects/index.js
@@ -1,21 +1,21 @@
 const projects = [
     {
         name: 'StayBuzzin',
-        description: 'Something will go here',
+        description: 'A simple social media app, sign up, make a post, make some friends, and have a great time.',
         repo: 'https://github.com/Landonwilson1/StayBuzzin.net',
         url: 'https://staybuzzin.herokuapp.com/',
     },
     {
         name: 'VocabQuiz',
-        description: 'Something will go here',
+        description: 'A fun little vocabulary learning quiz, jeopardy/hangman mix-up, great for children to learn new words whenever they would like.',
         repo: 'https://github.com/Natallie01/vocab-quiz',
         url: 'https://natallie01.github.io/vocab-quiz/',
     },
     {
         name: 'Proximo',
-        description: 'Something will go here',
+        description: 'A proximity based chat app, where users within a certain radius can chat anonymously or add other users as friends for private chatting.',
         repo: 'https://github.com/iiTONELOC/proximo',
-        url: 'https://natallie01.github.io/vocab-quiz/',
+        url: 'https://blooming-wildwood-24520.herokuapp.com/',
     }
 ];
 
